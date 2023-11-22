cfc428296d48c12f572d8d9274b081bef4660f98
Maria Jose Castrezana Molina <mjcm182009@gmail.com>
2023-11-21 20:48:27 -0600

    Initial commit

---
+++ Tipos de Datos Simples.py
@@ -0,0 +1,11 @@
+#Ejercicio 1
+#Escribir un programa que muestre por pantalla la cadena ¡Hola Mundo!.
+
+print('Hola Mundo')
+
+#Ejercicio 2
+#Escribir un programa que almacene la cadena ¡Hola Mundo! en una variable y luego muestre por pantalla el contenido de la variable.
+
+var1= 'Hola Mundo'
+print(var1)
+
